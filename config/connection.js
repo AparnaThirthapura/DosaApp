7c3fff5a64c7cef9ef074f6a9e4827ba3d1f0d22
Aparna Thirthapura <aparnathirthapura@Aparnas-MacBook-Air.local>
2017-04-29 19:56:33 -0400

    config changed

--- config/connection.js
+++ config/connection.js
@@ -1,58 +1,18 @@
-// Set up MySQL connection.
-// var mysql = require("mysql");
+var mysql = require("mysql");
+var connection;
 
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "dosaDB"
-// });
-
-// Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
-
-var mysql = require('mysql'),
-    connection,
-    source = {
-        localhost: {
-            port: 3306,
-            host: 'localhost',
-            user: 'root', // Your MySql username
-            password: '', // Your MySql password
-            database: 'dosaDB' 
-        },
-        jawsDB: {
-            port: 3306,
-            host: 'otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-            user: 'ew8shbbo9f1kolya',
-            password: 'imyeqh0repn6alqr',
-            database: 'h316gwycuwo8xdk1'
-        }
-    };
-
-// connect to localhost if jawsdb doesn't exist
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(source.jawsDB);
-} else {
-    connection = mysql.createConnection(source.localhost);
+if(process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
+else{
+    connection = mysql.createConnection({
+        host:"localhost",
+        user:"root",
+        password:"",
+        database:"dosaDB"
+    });
+};
 
-// Connect!!
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('Databased connected as id: ' + connection.threadId);
-});
+connection.connect();
 
-module.exports = connection;
-// Export connection for our ORM to use.
 module.exports = connection;
